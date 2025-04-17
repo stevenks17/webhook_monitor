7d04c95e55edb4c4dfa403993d0125a8b9ab4b44
steven <stevenks17@gmail.com>
2025-04-17 00:39:50 -0400

    fix: refine kafka producer to enable retries

--- app/kafka/producer.py
+++ app/kafka/producer.py
@@ -1,4 +1,5 @@
-from confluent_kafka import Producer
+from confluent_kafka import Producer, KafkaException
+import time
 import os
 import json
 
@@ -8,6 +9,15 @@ producer = Producer({
   "bootstrap.servers": KAFKA_BROKER
 })
 
-def publish_to_kafka(topic:str, message:dict):
-  producer.produce(topic, value=json.dumps(message).encode("utf-8"))
-  producer.flush()
+def publish_to_kafka(topic, message, retries=5):
+    for attempt in range(retries):
+        try:
+            producer.produce(topic, json.dumps(message).encode("utf-8"))
+            producer.flush()
+            break
+        except KafkaException as e:
+            if attempt < retries - 1:
+                print(f"Kafka not ready, retrying... ({attempt+1})")
+                time.sleep(2)
+            else:
+                raise e
